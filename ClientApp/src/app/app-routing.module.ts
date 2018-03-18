3caca6460e8b69c6a342f7e10b865a0d53b1e55e
Bradley Moorfield <brad.moorfield@gmail.com>
2018-03-18 18:29:56 +1300

    Fix import in app routing module

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 
-import { FacebookLoginComponent } from './facebook-login/facebook-login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Route[] = [
