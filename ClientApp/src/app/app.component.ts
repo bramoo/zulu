3b9ee344039c6dc29434afa33acb3d0d43a95dbc
Bradley Moorfield <brad.moorfield@gmail.com>
2018-04-07 15:07:54 +1200

    Fix css loading on app component
    Moved css to be inline in component

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ["@media (max-width: 767px) { /* On small screens, the nav menu spans the full width of the screen. Leave a space for it. */    .body - content {    padding-top: 50px;  }}"]
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
